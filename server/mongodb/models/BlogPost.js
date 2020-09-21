93d1317411e7193644ff9e55ac7339feda7c3e20
Matthew Bernet <mattbernetGT@gmail.com>
2020-09-20 21:01:29 -0400

    Create blog route branch, minor changes to blog post schema

---
+++ server/mongodb/models/BlogPost.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const BlogPostSchema = new Schema({
+  author: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  body: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export default mongoose.model("BlogPost", BlogPostSchema);
