3bdd7281146380fec1a004a44e3745ed5f7470ad
Dheeraj Bandaru <BandaruDheeraj@users.noreply.github.com>
2020-09-25 13:39:16 -0400

    method for blog list

--- src/screens/BlogTable/index.jsx
+++ src/screens/BlogTable/index.jsx
@@ -1,18 +1,26 @@
 import React from "react";
 import BlogTable from "../../components/Blogs/BlogTable";
+import Link from "next/link";
 
-const blogs = () => {
-  // Add button that links to admin/blogs/new.jsx to the right of the header
-
+const getBlogs = () => {
+  var sampleBlogList = [];
   //Make call to database pass into blog component and create a list of blogs
   //sample blog list to pass into table
 
+
   //Sample blog list for sprint 2
-  var sampleBlogList = [];
+  
   for (var i = 0; i < 4; i++) {
     sampleBlogList.push({ id: i + "" });
   }
 
+  return sampleBlogList;
+};
+
+
+const blogs = () => {
+  var sampleBlogList = getBlogs();
+  
   // Call table component which uses react beautiful DND
   return <BlogTable blogs={sampleBlogList} />;
 };
