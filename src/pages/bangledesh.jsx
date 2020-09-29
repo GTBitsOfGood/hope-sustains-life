68363fabf5d1c54b147816a5bcceeadcefd82474
jbaldwin2014 <jbaldwin2014@fau.edu>
2020-09-29 19:33:26 -0400

    Initial commit, changed index page title text, added images and blank pages for tanzania, bangledesh, haiti

---
+++ src/pages/bangledesh.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Bangledesh = () => <h1>Bangledesh!</h1>;
+
+export default Bangledesh;
---
+++ src/pages/tanzania.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Tanzania = () => <h1>Tanzania!</h1>;
+
+export default Tanzania;
