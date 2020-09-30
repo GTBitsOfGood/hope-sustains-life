5549aed002017a8a424e2071e0ce8536b4ff347d
Matthew Bernet <mattbernetGT@gmail.com>
2020-09-30 02:21:09 -0400

    Update user backend, not tested yet but should work

--- src/actions/User.js
+++ src/actions/User.js
@@ -90,3 +90,27 @@ export const getCurrentUser = (cookies) => {
       return json.payload;
     });
 };
+
+export const updateCurrentUser = (email, password) =>
+  fetch(urls.baseUrl + urls.api.user.updateCurrent, {
+    method: "PATCH",
+    mode: "same-origin",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email,
+      password,
+    }),
+  })
+    .then((response) => response.json())
+    .then((json) => {
+      if (json == null) {
+        throw new Error("Could not connect to API!");
+      } else if (!json.success) {
+        throw new Error(json.message);
+      }
+
+      return json.payload;
+    });
