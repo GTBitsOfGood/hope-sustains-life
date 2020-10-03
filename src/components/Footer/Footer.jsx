51b0d92a7825c114e9fb0d8443f2e96f46332887
Dheeraj Bandaru <BandaruDheeraj@users.noreply.github.com>
2020-10-03 14:46:08 -0400

    updated footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,17 +8,37 @@ import {
 } from "react-bootstrap";
 import styles from "./Footer.module.css";
 import logo from "./non-white globe-green_.png";
+import { Link, Router } from "react-router-dom";
+
 
 
 const Footer = () => {
   return (
     <div className={styles.footer}>
-      <Image src={logo}></Image>
-
+      
       <Container>
-        <Button href="/admin" className="btn btn-primary" type="submit">
-          Log In
-        </Button>
+        <Row>
+          <Col>
+            <Image src={logo}></Image>
+          </Col>
+
+          
+          <Col>
+            
+          </Col>
+
+          <Col md="auto">
+            <a href="/ourwork">OUR WORK</a>
+          </Col>
+
+          <Col>
+            <Button href="/admin" className="btn btn-primary" type="submit">
+              Log In
+            </Button>
+          </Col>
+
+          
+        </Row>
       </Container>
     </div>
   );
