f970c67e3cd9034d7c8f0c1fa8064ea3df3464e4
Dheeraj Bandaru <BandaruDheeraj@users.noreply.github.com>
2020-10-03 16:43:03 -0400

    Footer complete

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,6 +9,13 @@ import {
 import styles from "./Footer.module.css";
 import logo from "./non-white globe-green_.png";
 import { Link, Router } from "react-router-dom";
+import facebookLogo from './facebook.svg';
+import instagramLogo from './instagram.svg';
+import twitterLogo from './twitter.svg';
+import youtubeLogo from './youtube.svg';
+
+
+
 
 
 
@@ -19,25 +26,49 @@ const Footer = () => {
       <Container>
         <Row>
           <Col>
-            <Image src={logo}></Image>
+            <div className={styles.image}>
+              <Image src={logo}></Image>
+            </div>
           </Col>
 
           
           <Col>
+            <div className={styles.header}>ABOUT US</div>
+            <div className={styles.sublinks} href="/ourstory">OUR STORY</div>
+            <div className={styles.sublinks} href="/blognews">BLOG/NEWS</div>
+            <div className={styles.sublinks} href="/ourstory">OUR STORY</div>
             
           </Col>
 
-          <Col md="auto">
-            <a href="/ourwork">OUR WORK</a>
+          <Col>
+            <div className={styles.header} href="/ourwork">OUR WORK</div>
           </Col>
 
           <Col>
-            <Button href="/admin" className="btn btn-primary" type="submit">
-              Log In
-            </Button>
+            <div className={styles.header}>GET INVOLVED</div>
+            <div className={styles.sublinks} href="/joinclassroo">JOIN THE CLASSROOM</div>
+            <div className={styles.sublinks} href="/joinlibrary">JOIN THE LIBRARY</div>
           </Col>
 
-          
+          <Col>
+            <div className={styles.social}>FOLLOW US</div>
+            <Row>
+              <div className={styles.icon}>
+                <Image src = {facebookLogo}></Image>
+              </div>
+              <div className={styles.icon}>
+                <Image src = {instagramLogo}></Image>
+              </div>
+              <div className={styles.icon}>
+                <Image src = {twitterLogo}></Image>
+              </div>
+              <div className={styles.icon}>
+                <Image src = {youtubeLogo}></Image>
+              </div>
+            </Row>
+          </Col>
+
+
         </Row>
       </Container>
     </div>
