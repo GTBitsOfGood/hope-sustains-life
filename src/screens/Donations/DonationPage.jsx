3e5d77e76eae162c9173cfd2d26481940ac74970
Varshita Patakottu <varshita13@gmail.com>
2020-10-03 13:10:14 -0400

    created donation page

---
+++ src/screens/Donations/DonationPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./DonationPage.module.css";
+const DonationPage = () => {
+  return (
+    <div className={styles.container}>
+      <h1>yo</h1>
+    </div>
+  );
+};
+
+export default DonationPage;
