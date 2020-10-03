5e96114a53eb1c4289b8cc73a732276827d6e53d
Dheeraj Bandaru <BandaruDheeraj@users.noreply.github.com>
2020-10-03 14:29:52 -0400

    init commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,20 @@
 import React from "react";
-import { Button, Container } from "react-bootstrap";
+import {
+  Button,
+  Image,
+  Container,
+  Row,
+  Col,
+} from "react-bootstrap";
 import styles from "./Footer.module.css";
+import logo from "./non-white globe-green_.png";
+
 
 const Footer = () => {
   return (
     <div className={styles.footer}>
+      <Image src={logo}></Image>
+
       <Container>
         <Button href="/admin" className="btn btn-primary" type="submit">
           Log In
