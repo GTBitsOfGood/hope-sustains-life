a33c6b05edda1e1d21d987780cbfed683a953056
MeghanKulkarni <kulkarni.meg@gmail.com>
2020-10-12 13:06:31 -0400

    made admin blog table prettier (#53)
    * made admin blog table prettier
    
    * Fixed the awkward displacement while dragging
    
    Co-authored-by: Sho Szczepaniuk <sszczepaniuk3@gatech.edu>

--- src/components/Blogs/BlogTableRow.jsx
+++ src/components/Blogs/BlogTableRow.jsx
@@ -3,22 +3,42 @@ import PropTypes from "prop-types";
 import { Draggable } from "react-beautiful-dnd";
 import Link from "next/link";
 import classes from "./blog.module.css";
+import { Button } from "react-bootstrap";
 
 const BlogTableRow = ({ blog, index, onDeleteClick }) => {
   const { _id, title, date } = blog;
+  const getItemStyle = (isDragging) => ({
+    // change background colour if dragging
+    background: isDragging ? "lightgray" : "transparent",
+    borderLeft: isDragging ? "8px solid #03AB99" : "transparent",
+    borderBottom: "1px solid lightgray",
+    alignItems: "center",
+    display: "inline-flex",
+    maxWidth: "1279px",
+    maxHeight: "774px",
+    width: "90%",
+    height: "70%",
+    paddingLeft: "150px",
+    paddingRight: "0px",
+    paddingTop: "20px",
+    paddingBottom: "20px",
+    margin: "auto",
+  
+    // styles we need to apply on draggables
+    // ...draggableStyle
+  });
+
+
   return (
     <Draggable draggableId={blog._id} index={index}>
-      {(provided) => (
+      {(provided, snapshot) => (
         <div
           ref={provided.innerRef}
           {...provided.draggableProps}
           {...provided.dragHandleProps}
         >
-          <div className={classes.blogTableRow}>
+          <div style={getItemStyle(snapshot.isDragging)}>
             <div style={{ marginRight: 120, width: 300 }}>{title}</div>
-            <label style={{ marginRight: 90, width: 180 }}>
-              {new Date(date).toLocaleDateString()}
-            </label>
             <div className={classes.action}>
               <Link href={`/admin/blogs/${_id}`}>
                 <button className={classes.actionButtons}> View </button>
@@ -29,6 +49,9 @@ const BlogTableRow = ({ blog, index, onDeleteClick }) => {
               <button className={classes.actionButtons} onClick={onDeleteClick}>
                 Delete
               </button>
+            </div>
+            <div>
+            <Button className={classes.unpublishButton}>Unpublish</Button>
             </div>
           </div>
         </div>
