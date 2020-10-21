987e614a722766de9a3cb519d70d81bca0925f82
Varshita Patakottu <varshita13@gmail.com>
2020-10-21 13:24:06 -0400

    added image when viewing blogs

--- src/pages/admin/blogs/[id].jsx
+++ src/pages/admin/blogs/[id].jsx
@@ -1,26 +1,51 @@
 import React from "react";
 import Link from "next/link";
 import PropTypes from "prop-types";
+import { Image } from "cloudinary-react";
 import { getBlogById } from "../../../actions/Blog";
 import urls from "../../../../utils/urls";
+import styles from "./ViewBlog.module.css";
 
 const ViewPost = ({ blogPost }) => {
   return (
-    <>
+    <div>
       <Link href={urls.pages.admin.blogs}>
-        <button>Back</button>
+        <button className={styles.backButton}>
+          <i className={styles.arrow}></i>
+          BACK
+        </button>
       </Link>
-      <h1>{blogPost?.title}</h1>
-      <br />
-      <div dangerouslySetInnerHTML={{ __html: blogPost?.body }} />
-    </>
+      <br></br>
+      {blogPost?.image && (
+        <Image
+          className={styles.imageFormat}
+          cloudName="dh7fnazjb"
+          publicId={blogPost?.image.url}
+          secure="true"
+        ></Image>
+      )}
+      <div className={styles.headAndBodyContainer}>
+        <h1 style={{ textAlign: "center", fontWeight: "bold" }}>
+          {blogPost?.title}
+        </h1>
+        <br />
+        <div dangerouslySetInnerHTML={{ __html: blogPost?.body }} />
+        {blogPost?.references && (
+          <div>
+            <label style={{ fontWeight: "bold", fontSize: 18 }}>
+              REFERENCES
+            </label>
+            <div dangerouslySetInnerHTML={{ __html: blogPost?.references }} />
+          </div>
+        )}
+      </div>
+    </div>
   );
 };
 
 ViewPost.getInitialProps = async ({ query }) => {
   try {
     const blogPost = await getBlogById(query.id);
-
     return {
       blogPost,
     };
