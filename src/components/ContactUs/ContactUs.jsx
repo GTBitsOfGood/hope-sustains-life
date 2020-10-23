c369ce7f17050e2798cbe1f69c520923490b96af
Anneliese Blevins <annelieseblevins@gmail.com>
2020-10-22 23:10:13 -0400

    styling for ContactUs card

--- src/components/ContactUs/ContactUs.jsx
+++ src/components/ContactUs/ContactUs.jsx
@@ -15,62 +15,67 @@ const ContactUs = () => {
   };
   return (
     <>
-    <div style={{alignItems:"center"}}>
-      <Card border="light" style={{width: '50rem', borderRadius: "30px", alignItems: "center"}} className={classes.container}>
-        <Card.Title style={{fontSize: "40px"}} className={classes.title}>Contact Us</Card.Title>
-        <p style={{width: "400px"}}>
-          If you have questions or just want to get in touch, use the form
-          below. We look forward to hearing from you!
-        </p>
-        <form onSubmit={handleSubmit}>
-          <div className={classes.input}>
-            <input
-              className={classes.inputText}
-              required
-              id="name"
-              type="name"
-              value={name}
-              size="51"
-              placeholder=" Name"
-              onChange={(event) => setName(event.target.value)}
-            />
-          </div>
-          <br/>
-          <div className={classes.input}>
-            <input
-              className={classes.inputText}
-              required
-              id="email"
-              type="email"
-              value={email}
-              size="51"
-              placeholder=" E-Mail"
-              onChange={(event) => setEmail(event.target.value)}
-            />
-          </div>
-          <br/>
-          <div className={classes.inputMessage}>
-            <textarea
-              className={classes.inputText}
-              required
-              id="Message"
-              type="Message"
-              value={message}
-              size="50"
-              cols="50"
-              rows="10"
-              placeholder=" Message"
-              onChange={(event) => setMessage(event.target.value)}
-            />
-          </div>
-          <br/>
-          <button className={classes.submit} type="submit">
-            Submit
-          </button>
-        </form>
-      </Card>
-    </div>
-      
+      <div style={{ alignItems: "center" }}>
+        <Card
+          border="light"
+          style={{ width: "40rem", borderRadius: "40px", alignItems: "center" }}
+          className={classes.container}
+        >
+          <Card.Title style={{ fontSize: "40px" }} className={classes.title}>
+            Contact Us
+          </Card.Title>
+          <p style={{ width: "420px"}}>
+            If you have questions or just want to get in touch, use the form
+            below. We look forward to hearing from you!
+          </p>
+          <form onSubmit={handleSubmit}>
+            <div className={classes.input}>
+              <input
+                className={classes.inputText}
+                required
+                id="name"
+                type="name"
+                value={name}
+                size="51"
+                placeholder=" Name"
+                onChange={(event) => setName(event.target.value)}
+              />
+            </div>
+            <br />
+            <div className={classes.input}>
+              <input
+                className={classes.inputText}
+                required
+                id="email"
+                type="email"
+                value={email}
+                size="51"
+                placeholder=" E-Mail"
+                onChange={(event) => setEmail(event.target.value)}
+              />
+            </div>
+            <br />
+            <div className={classes.inputMessage}>
+              <textarea
+                className={classes.inputText}
+                required
+                id="Message"
+                type="Message"
+                value={message}
+                size="50"
+                cols="50"
+                rows="10"
+                placeholder=" Message"
+                onChange={(event) => setMessage(event.target.value)}
+              />
+            </div>
+            <br />
+            <button className={classes.submit} type="submit">
+              Submit
+            </button>
+          </form>
+        </Card>
+      </div>
     </>
   );
 };
