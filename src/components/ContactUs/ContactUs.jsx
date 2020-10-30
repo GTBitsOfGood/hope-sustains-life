91f3498769a3ce90a98393c204208ffacded80a3
jbaldwin2014 <jbaldwin40@gatech.edu>
2020-10-30 01:42:11 -0400

    Fixed FAQ to display, some CSS possibly displaying improperly

--- src/components/ContactUs/ContactUs.jsx
+++ src/components/ContactUs/ContactUs.jsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { Card } from "react-bootstrap";
+import { Accordion, Image, Card, Button } from "react-bootstrap";
 import classes from "./ContactUs.module.css";
 import { sendContactEmail } from "../../actions/Email";
+import ContactUsImage from "../../../public/static/contactus.png";
 
 const ContactUs = () => {
   const [name, setName] = React.useState("");
@@ -17,6 +18,9 @@ const ContactUs = () => {
   };
   return (
     <>
+      <div>
+        <Image src={ContactUsImage} className={classes.pictures} fluid />
+		  </div>
       <div style={{ alignItems: "center" }}>
         <Card
           border="light"
@@ -78,6 +82,64 @@ const ContactUs = () => {
           </form>
         </Card>
       </div>
+      <div style={{ alignItems: "center" }}>
+        <Card
+          border="light"
+          style={{ width: "40rem", borderRadius: "40px", alignItems: "center" }}
+          className={classes.container}
+        >
+          <Card.Title style={{ fontSize: "40px" }} className={classes.title}>
+            FAQ
+          </Card.Title>
+		  <Accordion defaultActiveKey="0">
+			<Card>
+			  <Card.Header>
+				<Accordion.Toggle
+				  className={classes.question}
+				  as={Button}
+				  variant="link"
+				  eventKey="0"
+				>
+				  Where does my donation go?
+				</Accordion.Toggle>
+			  </Card.Header>
+			  <Accordion.Collapse eventKey="0">
+				<Card.Body>Answer 1</Card.Body>
+			  </Accordion.Collapse>
+			</Card>
+			<Card>
+			  <Card.Header>
+				<Accordion.Toggle
+				  className={classes.question}
+				  as={Button}
+				  variant="link"
+				  eventKey="1"
+				>
+				  What is the difference between the Classroom and the Library?
+				</Accordion.Toggle>
+			  </Card.Header>
+			  <Accordion.Collapse eventKey="1">
+				<Card.Body>Answer 2</Card.Body>
+			  </Accordion.Collapse>
+			</Card>
+			<Card>
+			  <Card.Header>
+				<Accordion.Toggle
+				  className={classes.question}
+				  as={Button}
+				  variant="link"
+				  eventKey="2"
+				>
+				  What is the difference between the Classroom and the Library?
+				</Accordion.Toggle>
+			  </Card.Header>
+			  <Accordion.Collapse eventKey="2">
+				<Card.Body>Answer 3</Card.Body>
+			  </Accordion.Collapse>
+			</Card>
+		  </Accordion>
+      </Card>
+		</div>
     </>
   );
 };
