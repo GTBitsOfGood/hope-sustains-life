9c55a17855b9c3ea4a82ed7c65843c25746664ea
Sho Szczepaniuk <sszczepaniuk3@gatech.edu>
2020-11-02 16:58:17 -0700

    Fixed image stretching

--- src/screens/ContactUs/ContactUsPage.jsx
+++ src/screens/ContactUs/ContactUsPage.jsx
@@ -4,12 +4,13 @@ import ContactFaq from "../..//components/ContactFaq";
 import styles from "./ContactUsPage.module.css";
 import ContactUsImage from "../../../public/static/contactus.png";
 import classes from "./ContactUsPage.module.css";
+import { Image } from "react-bootstrap";
 
 const ContactUsPage = () => {
   return (
     <>
       <div>
-        <img src={ContactUsImage} className={classes.picture} />
+        <Image fluid src={ContactUsImage} className={classes.picture} />
       </div>
       <div className={styles.mainContainer}>
         <ContactUs />
