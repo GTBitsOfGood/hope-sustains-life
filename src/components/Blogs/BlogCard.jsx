98bddeb711889b1b337616abd08c519377c81f89
ishan baniya <ibaniya98@gmail.com>
2020-11-19 15:53:44 -0500

    Update Recommended Blog Card

--- src/components/Blogs/BlogCard.jsx
+++ src/components/Blogs/BlogCard.jsx
@@ -1,29 +1,26 @@
 import React from "react";
 import PropTypes from "prop-types";
-import Link from "next/link";
 import styles from "./blog.module.css";
 import urls from "../../../utils/urls";
-import { Button, Card } from "react-bootstrap";
+import { Card } from "react-bootstrap";
 import DefaultImage from "../../../public/static/home-bg.jpg";
 
 const BlogCard = ({ blog }) => {
   const blogUrl = `${urls.pages.blogNews}/${blog._id}`;
   return (
-    <Link href={blogUrl}>
-      <Card className={styles.card}>
-        <Card.Img
-          variant="top"
-          src={blog.image ? blog.image.url : DefaultImage}
-        />
-        <Card.Body>
-          <Card.Title className={styles.title}>{blog.title}</Card.Title>
-          <Card.Text>{blog.subtitle}</Card.Text>
-          <Card.Link className={styles.readMore} href="#">
-            Read More ⇒
-          </Card.Link>
-        </Card.Body>
-      </Card>
-    </Link>
+    <Card className={styles.card}>
+      <Card.Img
+        variant="top"
+        src={blog.image ? blog.image.url : DefaultImage}
+      />
+      <Card.Body>
+        <Card.Title className={styles.title}>{blog.title}</Card.Title>
+        <Card.Text>{blog.subtitle}</Card.Text>
+        <Card.Link className={styles.readMore} href={blogUrl}>
+          Read More ⇒
+        </Card.Link>
+      </Card.Body>
+    </Card>
   );
 };
 
