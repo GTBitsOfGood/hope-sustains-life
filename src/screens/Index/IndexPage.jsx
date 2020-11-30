65ecaba71bf4b2a1359188515c365374151435de
Sho Szczepaniuk <sszczepaniuk3@gatech.edu>
2020-11-29 19:00:21 -0500

    Fixing attribute names

--- src/screens/Index/IndexPage.jsx
+++ src/screens/Index/IndexPage.jsx
@@ -105,18 +105,19 @@ const IndexPage = () => (
       <h1 className={classes.rightWord}>WE ARE</h1>
       <Image src={LineImage} />
     </div>
-  <iframe
-    src="https://www.mapscout.io/hopesustainslife"
-    style={{borderWidth: 0}}
-    name="mapscout"
-    scrolling="no"
-    frameborder="0"
-    marginheight="0px"
-    marginwidth="0px"
-    height="814px"
-    width="100%"
-    className ={classes.centerMap}
-    allowfullscreen/>
+    <iframe
+      src="https://www.mapscout.io/hopesustainslife"
+      style={{ borderWidth: 0 }}
+      name="mapscout"
+      scrolling="no"
+      frameBorder="0"
+      marginHeight="0px"
+      marginWidth="0px"
+      height="814px"
+      width="100%"
+      className={classes.centerMap}
+      allowFullScreen
+    />
 
     <div className={classes.space}></div>
 
