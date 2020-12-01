8f00240f5e05f760764635ff54fb9933aeb17829
vpatakottu <47004464+vpatakottu@users.noreply.github.com>
2020-12-01 13:45:15 -0500

    Feature/mom duty (#112)
    * created branch for mom duty
    
    * prettified the home page a little
    
    * prettified first half of page
    
    * prettyified mobile home page a little more
    
    * fixing merge conflicts
    
    * fixed home title page
    
    * fixed merge conflicts
    
    * prettified the bottom a little
    
    * finishing touches on mobile version of home page

--- src/screens/Index/IndexPage.jsx
+++ src/screens/Index/IndexPage.jsx
@@ -8,156 +8,338 @@ import ImpactImage from "../../../public/static/impact.png";
 import InnovationImage from "../../../public/static/innovation.png";
 import TransparencyImage from "../../../public/static/transparency.png";
 import LineImage from "../../../public/static/line-1.png";
-import OurWorkImage from "../../../public/static/ourWork2.png";
+import OurWorkImage from "../../../public/static/ourWork.png";
+import { displayMobileView } from "../../../utils/screen.js";
 import HomeImage from "../../../public/static/home-bg.jpg";
 import urls from "../../../utils/urls";
 
-const IndexPage = () => (
-  <>
-    <Card className="bg-dark text-white">
-      <Card.Img src={HomeImage} alt="Card image" />
-      <Card.ImgOverlay className={classes.homeOverlay}>
-        <Card.Title className={classes.homeTitle}>
-          HOPE SUSTAINS LIFE
-        </Card.Title>
-        <Card.Text className={classes.homeSubtitle}>
-          Educating the future, one student at a time
-        </Card.Text>
-        <Button
-          href={urls.pages.ourStory}
-          className="btn centerButton"
-          type="submit"
-          style={{
-            backgroundColor: "#00ae99",
-            borderColor: "#00ae99",
-          }}
+const IndexPage = () => {
+  const isMobile = () => {
+    const mobile = displayMobileView();
+    return mobile;
+  };
+
+  const mobileView = isMobile();
+  return (
+    <>
+      <Card
+        style={
+          mobileView
+            ? {
+                height: "90%",
+                width: "100%",
+                alignItems: "center",
+                marginBottom: "10%",
+              }
+            : {}
+        }
+        className="bg-dark text-white"
+      >
+        <Card.Img
+          style={
+            mobileView
+              ? {
+                  height: "100%",
+                  width: "300%",
+                }
+              : {}
+          }
+          src={HomeImage}
+          alt="Card image"
+        />
+        <Card.ImgOverlay className={classes.homeOverlay}>
+          <Card.Title
+            style={
+              mobileView
+                ? { fontSize: 35, lineHeight: 1, textAlign: "center" }
+                : {}
+            }
+            className={classes.homeTitle}
+          >
+            HOPE SUSTAINS LIFE
+          </Card.Title>
+          <Card.Text
+            style={mobileView ? { fontSize: 15 } : {}}
+            className={classes.homeSubtitle}
+          >
+            Educating the future, one student at a time
+          </Card.Text>
+          <Button
+            href={urls.pages.ourStory}
+            className="btn centerButton"
+            type="submit"
+            style={
+              mobileView
+                ? {
+                    backgroundColor: "#00ae99",
+                    borderColor: "#00ae99",
+                    width: "60%",
+                    alignSelf: "center",
+                  }
+                : {
+                    backgroundColor: "#00ae99",
+                    borderColor: "#00ae99",
+                  }
+            }
+          >
+            Learn More
+          </Button>
+        </Card.ImgOverlay>
+      </Card>
+      <div className={classes.space}></div>
+      <div className={mobileView ? "" : classes.images}>
+        <a href={urls.pages.tanzania}>
+          <Image src={TanzaniaImage} className="pictures" fluid />
+        </a>
+        <a href={urls.pages.bangledesh}>
+          <Image src={BangledeshImage} className="pictures" fluid />
+        </a>
+        <a href={urls.pages.haiti}>
+          <Image
+            style={mobileView ? { marginBottom: "20%" } : {}}
+            src={HaitiImage}
+            className="pictures"
+            fluid
+          />
+        </a>
+      </div>
+
+      <div className={classes.space}></div>
+
+      <div className={classes.ourPrinciples}>
+        <Image src={LineImage} />
+        <h1
+          style={
+            mobileView
+              ? {
+                  fontSize: 20,
+                  marginRight: -10,
+                  paddingLeft: 20,
+                }
+              : {}
+          }
+          className={classes.leftWord}
         >
-          Learn More
-        </Button>
-      </Card.ImgOverlay>
-    </Card>
-    <div className={classes.space}></div>
-    <div className={classes.images}>
-      <a href={urls.pages.tanzania}>
-        <Image src={TanzaniaImage} className="pictures" fluid />
-      </a>
-      <a href={urls.pages.bangledesh}>
-        <Image src={BangledeshImage} className="pictures" fluid />
-      </a>
-      <a href={urls.pages.haiti}>
-        <Image src={HaitiImage} className="pictures" fluid />
-      </a>
-    </div>
-
-    <div className={classes.space}></div>
-
-    <div className={classes.ourPrinciples}>
-      <Image src={LineImage} />
-      <h1 className={classes.leftWord}>OUR</h1>
-      <h1 className={classes.rightWord}>PRINCIPLES</h1>
-      <Image src={LineImage} />
-    </div>
-
-    <div className={classes.ourPrinciplesImages}>
-      <div className={classes.impactImage}>
-        <Image src={ImpactImage} className={"pictures"} />
-      </div>
-      <div className={classes.innovationImage}>
-        <Image src={InnovationImage} className="pictures" />
-      </div>
-      <div className={classes.transparencyImage}>
-        <Image src={TransparencyImage} className="pictures" />
-      </div>
-    </div>
-
-    <div className={classes.impactInnovationTransp}>
-      <h2 className={classes.impact}>Impact</h2>
-      <h2 className={classes.innovation}>Innovation</h2>
-      <h2 className={classes.transparency}>Transparency</h2>
-    </div>
-    <div className={classes.paragraphs}>
-      <div className={classes.impactP}>
-        <p>
-          We make sure to provide GPS locations and photos of the schools we
-          have built or partnered with to show the impact we have had on the
-          refugee population and the surrounding community
-        </p>
-      </div>
-      <div className={classes.innovationP}>
-        <p>
-          Traditional methods have not been working for many years. Our team
-          works on developing breakthrough solutions to build or support schools
-          for the refugee population.
-        </p>
-      </div>
-      <div className={classes.transparencyP}>
-        <p>
-          We make sure 100% of your money goes to funding our educational
-          projects for the refugee population.
-        </p>
-      </div>
-    </div>
-
-    <div className={classes.space}></div>
-    <div className={classes.ourPrinciples}>
-      <Image src={LineImage} />
-      <h1 className={classes.leftWord}>WHERE</h1>
-      <h1 className={classes.rightWord}>WE ARE</h1>
-      <Image src={LineImage} />
-    </div>
-    <iframe
-      src="https://www.mapscout.io/hopesustainslife"
-      style={{ borderWidth: 0 }}
-      name="mapscout"
-      scrolling="no"
-      frameBorder="0"
-      marginHeight="0px"
-      marginWidth="0px"
-      height="814px"
-      width="100%"
-      className={classes.centerMap}
-      allowFullScreen
-    />
-
-    <div className={classes.space}></div>
-
-    <div className={classes.ourPrinciples}>
-      <Image src={LineImage} />
-      <h1 className={classes.leftWord}>OUR</h1>
-      <h1 className={classes.rightWord}>WORK</h1>
-      <Image src={LineImage} />
-    </div>
-    <Card
-      style={{
-        width: "80%",
-        marginLeft: "auto",
-        marginRight: "auto",
-        marginTop: "40px",
-      }}
-      className="bg-dark text-white"
-    >
-      <Card.Img src={OurWorkImage} alt="Card image" />
-      <Card.ImgOverlay className={classes.donateCardOverlay}>
-        <Card.Title className={classes.titleText}>
-          Make A Difference.
-        </Card.Title>
-        <Card.Text className={classes.homeSubtitle}>
-          Read about what we're working on and how we plan to do it.
-        </Card.Text>
-        <Button
-          href={urls.pages.ourStory}
-          className="btn centerButton"
-          type="submit"
-          style={{
-            backgroundColor: "#00ae99",
-            borderColor: "#00ae99",
-          }}
+          OUR
+        </h1>
+        <h1
+          style={mobileView ? { fontSize: 20, paddingRight: 20 } : {}}
+          className={classes.rightWord}
         >
-          View More
-        </Button>
-      </Card.ImgOverlay>
-    </Card>
-  </>
-);
+          PRINCIPLES
+        </h1>
+        <Image src={LineImage} />
+      </div>
 
+      <div
+        className={
+          mobileView ? classes.mobileImages : classes.ourPrinciplesImages
+        }
+      >
+        <div className={mobileView ? "" : classes.impactImage}>
+          <Image
+            style={mobileView ? { marginLeft: "10%" } : {}}
+            src={ImpactImage}
+            className={"pictures"}
+          />
+          {mobileView && (
+            <>
+              <h2 className={classes.mobileImagesText}>Impact</h2>
+              <p className={classes.mobileImagesBody}>
+                We make sure to provide GPS locations and photos of the schools
+                we have built or partnered with to show the impact we have had
+                on the refugee population and the surrounding community
+              </p>
+            </>
+          )}
+        </div>
+        <div className={mobileView ? "" : classes.innovationImage}>
+          <Image
+            style={mobileView ? { marginLeft: "15%" } : {}}
+            src={InnovationImage}
+            className="pictures"
+          />
+          {mobileView && (
+            <>
+              <h2 className={classes.mobileImagesText}>Innovation</h2>
+              <p className={classes.mobileImagesBody}>
+                Traditional methods have not been working for many years. Our
+                team works on developing breakthrough solutions to build or
+                support schools for the refugee population.
+              </p>
+            </>
+          )}
+        </div>
+        <div className={mobileView ? "" : classes.transparencyImage}>
+          <Image
+            style={mobileView ? { marginLeft: "10%" } : {}}
+            src={TransparencyImage}
+            className="pictures"
+          />
+          {mobileView && (
+            <>
+              <h2 className={classes.mobileImagesText}>Transparency</h2>
+              <p className={classes.mobileImagesBody}>
+                We make sure 100% of your money goes to funding our educational
+                projects for the refugee population.
+              </p>
+            </>
+          )}
+        </div>
+      </div>
+
+      {!mobileView && (
+        <>
+          <div className={classes.impactInnovationTransp}>
+            <h2 className={classes.impact}>Impact</h2>
+            <h2 className={classes.innovation}>Innovation</h2>
+            <h2 className={classes.transparency}>Transparency</h2>
+          </div>
+          <div className={classes.paragraphs}>
+            <div className={classes.impactP}>
+              <p>
+                We make sure to provide GPS locations and photos of the schools
+                we have built or partnered with to show the impact we have had
+                on the refugee population and the surrounding community
+              </p>
+            </div>
+            <div className={classes.innovationP}>
+              <p>
+                Traditional methods have not been working for many years. Our
+                team works on developing breakthrough solutions to build or
+                support schools for the refugee population.
+              </p>
+            </div>
+            <div className={classes.transparencyP}>
+              <p>
+                We make sure 100% of your money goes to funding our educational
+                projects for the refugee population.
+              </p>
+            </div>
+          </div>
+        </>
+      )}
+
+      <div className={classes.ourPrinciples}>
+        <Image src={LineImage} />
+        <h1
+          style={
+            mobileView
+              ? { fontSize: 20, marginRight: -10, paddingLeft: 20 }
+              : {}
+          }
+          className={classes.leftWord}
+        >
+          WHERE
+        </h1>
+        <h1
+          style={mobileView ? { fontSize: 20, paddingRight: 20 } : {}}
+          className={classes.rightWord}
+        >
+          WE&nbsp;ARE
+        </h1>
+        <Image src={LineImage} />
+      </div>
+
+      <iframe
+        src="https://www.mapscout.io/hopesustainslife"
+        style={{ borderWidth: 0 }}
+        name="mapscout"
+        scrolling="no"
+        frameBorder="0"
+        marginHeight="0px"
+        marginWidth="0px"
+        height="814px"
+        width="100%"
+        className={classes.centerMap}
+        allowFullScreen
+      />
+
+      <div className={classes.space}></div>
+
+      <div
+        style={mobileView ? { marginBottom: "20%" } : {}}
+        className={classes.ourPrinciples}
+      >
+        <Image src={LineImage} />
+        <h1
+          style={
+            mobileView
+              ? { fontSize: 20, marginRight: -10, paddingLeft: 20 }
+              : {}
+          }
+          className={classes.leftWord}
+        >
+          OUR
+        </h1>
+        <h1
+          style={mobileView ? { fontSize: 20, paddingRight: 20 } : {}}
+          className={classes.rightWord}
+        >
+          WORK
+        </h1>
+        <Image src={LineImage} />
+      </div>
+
+      <Card
+        style={
+          mobileView
+            ? {
+                borderRadius: 0,
+                height: "35%",
+                alignItems: "center",
+              }
+            : {
+                width: "80%",
+                marginLeft: "auto",
+                marginRight: "auto",
+                marginTop: "40px",
+              }
+        }
+        className="bg-dark text-white"
+      >
+        <Card.Img
+          style={mobileView ? { height: "100%", width: "120%" } : {}}
+          src={OurWorkImage}
+          alt="Card image"
+        />
+        <Card.ImgOverlay className={classes.donateCardOverlay}>
+          <Card.Title
+            style={mobileView ? { fontSize: 15 } : {}}
+            className={classes.titleText}
+          >
+            Make A Difference.
+          </Card.Title>
+          <Card.Text
+            className={
+              mobileView ? classes.mobileHomeSubtitle : classes.homeSubtitle
+            }
+          >
+            Read about what we're working on and how we plan to do it.
+          </Card.Text>
+          <Button
+            href={urls.pages.ourStory}
+            className="btn centerButton"
+            type="submit"
+            style={
+              mobileView
+                ? {
+                    backgroundColor: "#00ae99",
+                    borderColor: "#00ae99",
+                    fontSize: 12,
+                    width: "95%",
+                  }
+                : {
+                    backgroundColor: "#00ae99",
+                    borderColor: "#00ae99",
+                  }
+            }
+          >
+            View More
+          </Button>
+        </Card.ImgOverlay>
+      </Card>
+    </>
+  );
+};
 export default IndexPage;
