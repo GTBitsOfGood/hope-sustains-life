4682d9ef6289546c8f6b56c767c25ca89109bf5b
ishan baniya <ibaniya98@gmail.com>
2020-12-09 12:29:18 -0500

    Fix mobile view for Join Classroom

--- src/screens/JoinClassroom/JoinClassroomPage.jsx
+++ src/screens/JoinClassroom/JoinClassroomPage.jsx
@@ -22,6 +22,13 @@ function JoinClassroomPage() {
   };
   const mobileView = isMobile();
 
+  const jumbotron = (
+    <div className={classes.jumbotron}>
+      <Image fluid src={MainPic} className={classes.picture} />
+      <h1 className={classes.pageName}>The Classroom</h1>
+    </div>
+  );
+
   const photoGrid = (
     <Row>
       <Col md={4} className="d-flex flex-column px-0">
@@ -42,156 +49,93 @@ function JoinClassroomPage() {
     </Row>
   );
 
+  const contents = (
+    <>
+      <h1 className={classes.title}>
+        Everyone <span className={classes.blueText}>welcome!</span>
+      </h1>
+      <br />
+      <p>
+        The Classroom is our community of donors. Their contributions are put
+        straight back into the project fund.
+      </p>
+      <p>
+        The Classroom is our group of public donors that are opening up
+        opportunities to the next generation of children. Their <br></br>
+        donations are put into the necessities that we take for granted: the
+        school, their school materials, and the meals. This <br></br>community
+        can follow as blogs are posted on world updates and how the Hope
+        Sustains Life projects are progressing.
+      </p>
+      <br />
+      <br />
+      <Col md={2}>
+        <h1 className={classes.greenUnderline}>
+          &emsp;&emsp;The <span className={classes.blueText}>Impact</span>
+        </h1>
+      </Col>
+      <br />
+      <br />
+      <p>
+        Education gives these young minds HOPE. Your contribution is an
+        investment in the astounding number of refugee <br></br>children out of
+        school. Imagine navigating the world without anything or anyone to
+        believe in you. Education paves a <br></br>brighter path to becoming
+        self-sufficient individuals that are capable of achieving incredible
+        feats, just as long as <br></br>they’re given the tools to harness those
+        strengths.
+      </p>
+      <br />
+      <br />
+      <Col md={2}>
+        <h1 className={classes.greenUnderline}>
+          &emsp;&emsp;How To <span className={classes.blueText}>Join</span>
+        </h1>
+      </Col>
+      <br />
+      <br />
+      <p>
+        Check out the Our Work page and donate to aid the projects that we’re
+        currently involved with. If you have any further <br></br>
+        questions, visit our Contact Us page and shoot us an email.
+      </p>
+    </>
+  );
+
   if (mobileView) {
     return (
-      <>
-        <div>
-          <div>
-            <Image fluid src={MainPic} className={classes.picture} />
-            <h1 className={classes.pageName}>The Classroom</h1>
-          </div>
-          <div className={styles.mainContainer}>
-            <div className={classes.card}>
-              <h1 className={classes.title}>
-                Everyone <span className={classes.blueText}>welcome!</span>
-              </h1>
-              <br />
-              <p>
-                The Classroom is our community of donors. Their contributions
-                are put straight back into the project fund.
-              </p>
-              <p>
-                The Classroom is our group of public donors that are opening up
-                opportunities to the next generation of children. Their{" "}
-                <br></br>donations are put into the necessities that we take for
-                granted: the school, their school materials, and the meals. This{" "}
-                <br></br>community can follow as blogs are posted on world
-                updates and how the Hope Sustains Life projects are progressing.
-              </p>
-              <br />
-              <br />
-              <Col md={2}>
-                <h1 className={classes.greenUnderline}>
-                  &emsp;&emsp;The{" "}
-                  <span className={classes.blueText}>Impact</span>
-                </h1>
-              </Col>
-              <br />
-              <br />
-              <p>
-                Education gives these young minds HOPE. Your contribution is an
-                investment in the astounding number of refugee <br></br>children
-                out of school. Imagine navigating the world without anything or
-                anyone to believe in you. Education paves a <br></br>brighter
-                path to becoming self-sufficient individuals that are capable of
-                achieving incredible feats, just as long as <br></br>they’re
-                given the tools to harness those strengths.
-              </p>
-              <br />
-              <br />
-              <Col md={2}>
-                <h1 className={classes.greenUnderline}>
-                  &emsp;&emsp;How To{" "}
-                  <span className={classes.blueText}>Join</span>
-                </h1>
-              </Col>
-              <br />
-              <br />
-              <p>
-                Check out the Our Work page and donate to aid the projects that
-                we’re currently involved with. If you have any further <br></br>
-                questions, visit our Contact Us page and shoot us an email.
-              </p>
-              <br />
-              <br />
-            </div>
-          </div>
-          <div className={classes.pictures}>{photoGrid}</div>
-          <br />
-          <br />
-          <div className={classes.background}>
-            <h1 className={classes.donateText}>
-              Don’t wait to change the<br></br>world...the world needs the{" "}
-              <br></br>change you can bring now
+      <div>
+        {jumbotron}
+        <div className={styles.mainContainer}>
+          <div className={classes.card}>{contents}</div>
+        </div>
+        <div className={classes.pictures}>{photoGrid}</div>
+
+        <div className={classes.background}>
+          <div className="d-flex flex-column justify-content-center">
+            <h1 className={classes.donateTextMobile}>
+              Don’t wait to change the world...the world needs the change you
+              can bring now
             </h1>
-            <div className={classes.buttonDiv}>
-              <button className={classes.donateButton}>Donate</button>
-            </div>
+            <button className={classes.donateButton}>Donate</button>
           </div>
         </div>
-      </>
+      </div>
     );
   } else {
     return (
       <>
         <div>
-          <div>
-            <Image fluid src={MainPic} className={classes.picture} />
-            <h1 className={classes.pageName}>The Classroom</h1>
-          </div>
+          {jumbotron}
           <div className={styles.mainContainer}>
-            <div className={classes.card}>
-              <h1 className={classes.title}>
-                Everyone <span className={classes.blueText}>welcome!</span>
-              </h1>
-              <br />
-              <p>
-                The Classroom is our community of donors. Their contributions
-                are put straight back into the project fund.
-              </p>
-              <p>
-                The Classroom is our group of public donors that are opening up
-                opportunities to the next generation of children. Their{" "}
-                <br></br>donations are put into the necessities that we take for
-                granted: the school, their school materials, and the meals. This{" "}
-                <br></br>community can follow as blogs are posted on world
-                updates and how the Hope Sustains Life projects are progressing.
-              </p>
-              <br />
-              <br />
-              <Col md={2}>
-                <h1 className={classes.greenUnderline}>
-                  &emsp;&emsp;The{" "}
-                  <span className={classes.blueText}>Impact</span>
-                </h1>
-              </Col>
-              <br />
-              <br />
-              <p>
-                Education gives these young minds HOPE. Your contribution is an
-                investment in the astounding number of refugee <br></br>children
-                out of school. Imagine navigating the world without anything or
-                anyone to believe in you. Education paves a <br></br>brighter
-                path to becoming self-sufficient individuals that are capable of
-                achieving incredible feats, just as long as <br></br>they’re
-                given the tools to harness those strengths.
-              </p>
-              <br />
-              <br />
-              <Col md={2}>
-                <h1 className={classes.greenUnderline}>
-                  &emsp;&emsp;How To{" "}
-                  <span className={classes.blueText}>Join</span>
-                </h1>
-              </Col>
-              <br />
-              <br />
-              <p>
-                Check out the Our Work page and donate to aid the projects that
-                we’re currently involved with. If you have any further <br></br>
-                questions, visit our Contact Us page and shoot us an email.
-              </p>
-              <br />
-              <br />
-            </div>
+            <div className={classes.card}>{contents}</div>
           </div>
           <div className={classes.pictures}>{photoGrid}</div>
-          <br />
-          <br />
           <div className={classes.background}>
             <h1 className={classes.donateText}>
               Don’t wait to change the<br></br>world...the world needs the{" "}
-              <br></br>change you can bring now
+              <br></br>
+              change you can bring now
             </h1>
             <div className={classes.buttonDiv}>
               <button className={classes.donateButton}>Donate</button>
