628e9b5d4ef4129d4084c6304eb8f18fd56380a0
Sho Szczepaniuk <37124216+shopaniuk@users.noreply.github.com>
2020-12-12 15:34:30 -0500

    Our Work mobile done (#120)

--- src/components/OurLocationCard/OurLocationCard.jsx
+++ src/components/OurLocationCard/OurLocationCard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Image, Button } from "react-bootstrap";
+import { Image, Button, Card } from "react-bootstrap";
 import classes from "./OurLocationCard.module.css";
 import Link from "next/link";
 
@@ -7,19 +7,45 @@ import Link from "next/link";
  * Component for Our Location Cards
  */
 
-const OurLocationCard = ({ image, title, location, navlink }) => {
-  return (
-    <div className={classes.roundedCard}>
-      <Image className={classes.icon} src={image} fluid rounded />
-      <div className={classes.locationDescription}>
-        <h2 className={classes.locationTitle}>{title}</h2>
-        <h1 className={classes.location}>{location}</h1>
-        <Link href={navlink}>
-          <Button className={classes.viewMore}>View More</Button>
-        </Link>
+const OurLocationCard = ({ image, title, location, navlink, isMobile = false }) => {
+  if (isMobile) {
+    return(
+    <Card className={classes.mobileCard}>
+      <Card.Img
+        src={image}
+        alt="Card image"
+      />
+        <Card.ImgOverlay className={classes.cardOverlay}>
+          <div>
+            <Card.Title className={classes.titleText}>
+              {title}
+            </Card.Title>
+            <Card.Text className={classes.locationText}>
+              {location}
+            </Card.Text>
+            <Link href={navlink}>
+              <Button className={classes.viewMore}>
+                View More
+        </Button>
+            </Link>
+          </div>
+        </Card.ImgOverlay>
+      </Card>
+    )
+  } else {
+    return (
+      <div className={classes.roundedCard}>
+        <Image className={classes.icon} src={image} fluid rounded />
+        <div className={classes.locationDescription}>
+          <h2 className={classes.locationTitle}>{title}</h2>
+          <h1 className={classes.location}>{location}</h1>
+          <Link href={navlink}>
+            <Button className={classes.viewMore}>View More</Button>
+          </Link>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default OurLocationCard;
