9b89f0f496aa1f9e0684bb4fa6a91f2024a53dec
Matt Bernet <46691358+mjbernet3@users.noreply.github.com>
2020-12-12 13:57:49 -0500

    Publish fix (#119)
    * Fix publishing edge cases
    
    * Linting

--- src/screens/EditBlog/EditBlogPage.jsx
+++ src/screens/EditBlog/EditBlogPage.jsx
@@ -1,37 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { useRouter } from "next/router";
 import BlogPostForm from "../../components/BlogPostForm";
-import { updateBlog } from "../../actions/Blog";
-import urls from "../../../utils/urls";
 
 const EditBlogPage = ({ blogPost }) => {
-  const router = useRouter();
-
-  const handleSavePublish = async (
-    title,
-    subtitle,
-    body,
-    references,
-    setPublished,
-    cloudinaryImage,
-    deleteOriginalImage
-  ) => {
-    updateBlog(
-      blogPost._id,
-      title,
-      subtitle,
-      body,
-      references,
-      setPublished || blogPost.isPublished, // If user clicks save, setPublished will be false so use previous value for the blog published
-      cloudinaryImage,
-      deleteOriginalImage // this flag is necessary if user does not add new image and deletes the previous one
-    ).then(() => router.replace(urls.pages.admin.blogs));
-  };
-
-  return (
-    <BlogPostForm handleSavePublish={handleSavePublish} blogPost={blogPost} />
-  );
+  return <BlogPostForm blogPost={blogPost} />;
 };
 
 EditBlogPage.propTypes = {
