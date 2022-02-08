2cbc87b447afbfcb6e34f7da127d226af16e23bb
mxinburritos <mxinburritos@gmail.com>
2022-02-07 22:51:35 -0500

    Finish password reset endpoint

--- server/mongodb/actions/ResetUser.js
+++ server/mongodb/actions/ResetUser.js
@@ -1,35 +1,27 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import mongoDB from "../index";
+import PasswordResetRequest from "../models/PasswordResetRequest";
 import User from "../models/User";
 
-export async function resetUser(id, password) {
+export async function resetUser(token, password) {
   if (password == null) {
     throw new Error("Password must be provided!");
   }
 
-  await mongoDB();
+  const hashedPassword = await bcrypt.hash(password, 10);
 
-  try {
-    const hashedPassword = await bcrypt.hash(password, 10);
+  const request = await PasswordResetRequest.find({ token: token });
 
-    const user = await User.findOneAndUpdate(
-      { _id: id },
-      { password: hashedPassword },
-      { new: true }
-    );
-
-    return jwt.sign(
-      {
-        id: user._id,
-        email: user.email,
-      },
-      process.env.JWT_SECRET,
-      {
-        expiresIn: "7d",
-      }
-    );
-  } catch (error) {
-    throw new Error("Invalid token!");
+  if (request.length === 0) {
+    throw new Error("Password reset query not found");
   }
+
+  await PasswordResetRequest.findOneAndDelete({ token: token });
+
+  await User.findOneAndUpdate(
+    { email: request[0].email },
+    { password: hashedPassword },
+    { new: false }
+  );
 }
