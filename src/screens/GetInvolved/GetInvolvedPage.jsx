f3b30cafe5ee5c065e8d1d90fb405948f3a9519d
Jay Nagpaul <jaynagpaul@users.noreply.github.com>
2023-04-09 14:32:41 -0400

    Fix CSS / HTML rendering

--- src/screens/GetInvolved/GetInvolvedPage.jsx
+++ src/screens/GetInvolved/GetInvolvedPage.jsx
@@ -1,56 +1,41 @@
 import React from "react";
-import classes from "./GetInvovledPage.module.css";
+import classes from "./GetInvolvedPage.module.css";
 import urls from "../../../utils/urls";
-import Link from "next/link";
-import { Button } from "react-bootstrap";
 import NavCard from "./NavCard";
 
 function GetInvolvedPage() {
-
-  const contents = (
-    <>
-      <div className={classes.title}>
-        Their education is fueled by your generosity
-      </div>
-      <div className={classes.grid}>
-        <NavCard
-          img="/static/BangladeshImage.png"
-          link={urls.pages.joinClassroom}
-          content={
-            <p>
-              Join a community of donors that directly fund the something of a
-              classroom
-            </p>
-          }
-          buttonData={"Fund a Classroom"}
-        />
-        <NavCard
-          img="/static/libraryHeaderPic.png"
-          link={urls.pages.joinLibrary}
-          content={
-            <p>Join the library - a group of donors that help ADDD MOREEE</p>
-          }
-          buttonData={"Join the Library"}
-        />
-        <NavCard
-          img="/static/BangladeshImage.png"
-          link={urls.pages.joinLibrary}
-          content={
-            <p>
-              Learn more about other ways to get involved with Hope Sustains
-              Life
-            </p>
-          }
-          buttonData={"Other Opportunities"}
-        />
-      </div>
-    </>
-  );
-
   return (
     <div className={classes.screen}>
       <div className={classes.mainContainer}>
-        <div>{contents}</div>
+        <div className={classes.title}>
+          Their education is fueled by your generosity
+        </div>
+        <div className={classes.grid}>
+          <NavCard
+            img="/static/BangladeshImage.png"
+            link={urls.pages.joinClassroom}
+            content={
+              "Join a community of donors that are opening up opportunities to the next generation of children."
+            }
+            buttonData={"Fund a Classroom"}
+          />
+          <NavCard
+            img="/static/libraryHeaderPic.png"
+            link={urls.pages.joinLibrary}
+            content={
+              "Join a group of private donors who assist with advancing the vision of Hope Sustains Life and also make sure we are meeting our 100% transparency promise."
+            }
+            buttonData={"Join the Library"}
+          />
+          <NavCard
+            img="/static/BangladeshImage.png"
+            link={urls.pages.joinLibrary}
+            content={
+              "Learn more about other ways to get involved with Hope Sustains Life."
+            }
+            buttonData={"Other Opportunities"}
+          />
+        </div>
       </div>
     </div>
   );
